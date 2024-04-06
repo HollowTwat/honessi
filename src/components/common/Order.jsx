34a2b1f8b6ba4ee51759bc72ae57604b74dc6325
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-07 01:39:35 +0300

    Update Order.jsx

--- src/components/common/Order.jsx
+++ src/components/common/Order.jsx
@@ -46,7 +46,12 @@ const Order = ({type}) => {
 
         tg.showPopup({
                 title: 'Оформление заказа',
-                message: 'Вы уверены что хотите офрмить заказ?',
+                message: 'Вы уверены что хотите офрмить заказ?'+JSON.stringify({
+                    category: type,
+                    user_name: user?.username,
+                    user_id: user?.id.toString(),
+                    order: order
+                }),
                 buttons: [
                     {id: 1, type: 'default', text: 'Да'},
                     {id: 2, type: 'default', text: 'Нет'}
