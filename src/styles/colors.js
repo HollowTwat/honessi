c6df75d7db4d649b8b1ec0859bbd42633add1be3
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-06 23:58:31 +0300

    Update colors.js

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,16 +1,16 @@
-import { useTelegram } from "../hooks/useTelegram"
-import{
-    tgbutton,
-    tgbuttontext,
-    tgtextcolor
-} from useTelegram
+// import { useTelegram } from "../hooks/useTelegram"
+// import{
+//     tgbutton,
+//     tgbuttontext,
+//     tgtextcolor
+// } from useTelegram
     
     
     
-export const buttontext = tgbuttontext
-export const textcolor = tgtextcolor
-export const mainBlue = tgbutton
-//export const mainBlue = '#BB86FC'
+// export const buttontext = tgbuttontext
+// export const textcolor = tgtextcolor
+// export const mainBlue = tgbutton
+export const mainBlue = '#BB86FC'
 export const mainBlue = '#0d5ffe'
 export const mainYellow = '#e5d022'
 export const addOrEditPositionButton = '#0d5ffe'
