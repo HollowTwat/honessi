e54131678037651ac234d87a273e02a6ec1db86e
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-08 00:00:06 +0300

    Update useServer.js

--- src/hooks/useServer.js
+++ src/hooks/useServer.js
@@ -40,12 +40,7 @@ const useServer = () => {
                     "Content-Type": "application/json",
                     "Accept": "application/json",
                 },
-                    body: JSON.stringify({
-                        category: type, 
-                        user_name: user?.username, 
-                        user_id: user?.id.toString(), 
-                        order: order
-                        })
+                    body: JSON.stringify({category: type, user_name: user?.username, user_id: user?.id.toString(), order: order})
                     });
 
             const result = await response.json();
@@ -57,7 +52,7 @@ const useServer = () => {
                 order_id: ''
             }
         }
-    }
+    };
 
     return {sendNewOrder,error,};
 
