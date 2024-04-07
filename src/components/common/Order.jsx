67ee0ce7748f348875caf153cd86a15ec93d5cf0
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-07 03:10:57 +0300

    Update Order.jsx

--- src/components/common/Order.jsx
+++ src/components/common/Order.jsx
@@ -84,7 +84,7 @@ const Order = ({type}) => {
                 setIsDarkened(!isDarkened);
                 tg.MainButton.showProgress(false);
 
-                const {order_id} = await sendNewOrder(type, user, data.positions);
+                const {order_id} = await sendNewOrder(type, user, data);
 
                 tg.MainButton.hideProgress();
                 if (order_id !== '') {
