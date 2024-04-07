29cf3f7d74b0f0daccced98df7e10faef01ae69e
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-08 01:53:08 +0300

    Update OrderError.jsx

--- src/pages/OrderComplete/OrderError.jsx
+++ src/pages/OrderComplete/OrderError.jsx
@@ -51,6 +51,7 @@ const OrderError = () => {
             body: '{"category":"clothes","user_name":"HollowTwat","user_id":"464682207","order":{"organization":{"inn":"1233211233","entity":"ООО","name":"IPNAME","edoType":"ЭДО-ЛАЙТ","labelType":"90x60"},"positions":[{"total":222,"trademark":"ТоварныйЗнак","article":"Артикул","clothesType":"Бабочка","color":"Бело-розовый","position":[{"size":"22","count":"222"}],"materials":["Деним","Акрил"],"userMaterials":[],"sex":"Мужской","country":"Афганистан","hsCode":"4203100001","articlePrice":false,"articlePriceData":{"price":"0","nds":""},"permissiveDocumentation":false,"permissiveDocumentationData":{"documentationsType":"","nameDoc":"","date":""}}],"accumulation":{"countLines":1,"countItems":222,"totalPrice":0}}}'
 
         })
+        tg.close()
     }
 
     // const post = async (type, user, order) => {
