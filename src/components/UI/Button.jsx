a12897c37da2813048227f1dd0697c84516871ee
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-10 01:32:16 +0300

    Update Button.jsx

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 // import {mainYellow} from "../../styles/colors";
 import {button} from "../../styles/button";
-import tg from "../../hooks/useTelegram";
 
-const Button = ({background = tg.ThemeParams.bg_color, textColor = 'black', ...props}) => {
+const Button = ({background = window.Telegram.WebApp.ThemeParams.bg_color, textColor = 'black', ...props}) => {
 
     return (
         <button {...props} className={'button'} style={{backgroundColor: background, color: textColor, ...button}}/>
