2e57766d1ad5b12881169fade465262ce78e33ce
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-10 00:39:01 +0300

    Update Button.jsx

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {tg_bttn,textcolor} from "../../styles/colors";
 import {button} from "../../styles/button";
 
-const Button = ({background = tg_bttn, textColor = textcolor, ...props}) => {
+const Button = ({background = tg_bttn, textColor = buttontext, ...props}) => {
 
     return (
         <button {...props} className={'button'} style={{backgroundColor: background, color: textColor, ...button}}/>
