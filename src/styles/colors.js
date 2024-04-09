326715cec43fc79dd3830cd11db23c45394dc6df
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-09 23:02:05 +0300

    Update colors.js

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -8,7 +8,7 @@ import{
 export const buttontext = tgbuttontext
 export const textcolor = tgtextcolor
 export const mainBlue = '#0d5ffe'
-export const mainYellow = '#e5d022'
+export const mainYellow = tgbutton //'#e5d022'
 export const addOrEditPositionButton = '#0d5ffe'
 export const orderButton = '#00C600'
 export const unActiveButton = '#dddddd'
