cdb9dd925ec87fbc9d1c0d40e61ca334de98dfb2
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-10 00:37:31 +0300

    Update useTelegram.js

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,10 +1,10 @@
 const tg = window.Telegram.WebApp;
 
-export const tgbg = tg.bg_color;
-export const tgbutton = tg.button_color;
-export const tgbutton2 = tg.secondary_bg_color;
-export const tgbuttontext = tg.button_text_color;
-export const tgtextcolor = tg.text_color;
+export const tgbg = tg.ThemeParams.bg_color;
+export const tgbutton = tg.ThemeParams.button_color;
+export const tgbutton2 = tg.ThemeParams.secondary_bg_color;
+export const tgbuttontext = tg.ThemeParams.button_text_color;
+export const tgtextcolor = tg.ThemeParams.text_color;
 
 export function useTelegram() {
 
