a13c185c34719e214b1a9468dc6fc15ffadcd569
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-25 17:09:50 +0300

    Update Button with themes.jsx

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,8 +1,21 @@
 import React from "react";
-import {mainYellow} from "../../styles/colors";
+// import {mainYellow} from "../../styles/colors";
 import {button} from "../../styles/button";
+import {theme, themestate} from '../../styles/theme'
 
-const Button = ({background = mainYellow, textColor = 'black', ...props}) => {
+// state = {
+//     themeMode: themestate
+// };
+
+// toggleTheme = () =>{
+//     this.setState({themeMode: this.state.themeMode === 'light' ? 'dark': 'dark'});
+// };
+
+
+// theme = theme[this.state.themeMode]
+
+// const Button = ({background = mainYellow, textColor = 'black', ...props}) => {
+const Button = ({background = theme[themestate].body, textColor = theme[themestate].text, ...props}) => {
 
     return (
         <button {...props} className={'button'} style={{backgroundColor: background, color: textColor, ...button}}/>
