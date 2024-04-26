15649fcc5b6b7bd603daa5989674a0ed94b4e9e2
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-26 17:19:09 +0300

    Update MultipleSelect.jsx

--- src/components/UI/MultipleSelect.jsx
+++ src/components/UI/MultipleSelect.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Checkbox, FormControl, InputLabel, ListItemText, MenuItem, OutlinedInput, Select, Button } from "@mui/material";
+import { pink, purple, red } from '@mui/material/colors';
 
 const MultipleSelect = ({ label = '', options = [], initValue = [], onChange = () => {}, onChangeValid = () => {} }) => {
     const [values, setValues] = useState(initValue);
@@ -8,12 +9,16 @@ const MultipleSelect = ({ label = '', options = [], initValue = [], onChange = (
     useEffect(() => {
         onChange(values);
         onChangeValid(values.length > 0);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [values]);
+    }, [values, onChange, onChangeValid]);
 
     const handleChange = (event) => {
-        const { target: { value } } = event;
-        setValues(typeof value === 'string' ? value.split(',') : value);
+        const {
+            target: { value },
+        } = event;
+        setValues(
+            // On autofill we get a the stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
     };
 
     const handleClose = () => {
@@ -27,14 +32,34 @@ const MultipleSelect = ({ label = '', options = [], initValue = [], onChange = (
             marginBottom: 10,
         }}>
             <FormControl fullWidth>
-                <InputLabel>{label}</InputLabel>
+                <InputLabel sx={{ color: red.A700 }}>{label}</InputLabel>
                 <Select
                     multiple
+                    sx={{color: red.A700}}
                     value={values}
                     onChange={handleChange}
                     input={<OutlinedInput label={label} />}
                     renderValue={(selected) => selected.join(', ')}
                     MenuProps={{
+                        PaperProps: {
+                            sx: {
+                                bgcolor: 'var(--tg-theme-bg-color)'//pink[50], // Sets the dropdown background color
+                            }
+                        },
+                        sx: {
+                            '&& .MuiMenuItem-root': { // Stronger specificity for default style
+                                color: red.A700,
+                                '&:hover': {
+                                    backgroundColor: purple[100], // Background color for items on hover
+                                },
+                                '&.Mui-selected': {
+                                    backgroundColor: pink[100], // Background color for the selected item
+                                    '&:hover': {
+                                        backgroundColor: purple[100], // Background color for the selected item on hover
+                                    }
+                                }
+                            }
+                        },
                         onClose: handleClose // Call handleClose when the menu is closed
                     }}
                     open={menuOpen}
@@ -42,11 +67,11 @@ const MultipleSelect = ({ label = '', options = [], initValue = [], onChange = (
                 >
                     {options.map((option) => (
                         <MenuItem key={option} value={option}>
-                            <Checkbox checked={values.indexOf(option) > -1} sx={{ width: "unset" }} />
+                            <Checkbox checked={values.indexOf(option) > -1} sx={{ color: pink[600], width: 'unset' }} />
                             <ListItemText primary={option} />
                         </MenuItem>
                     ))}
-                    <MenuItem>
+                <MenuItem>
                         <Button onClick={handleClose} variant="contained" color="primary">
                             Ok
                         </Button>
