2d56e524dc989a05f0793f9f013399954f130946
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-26 18:20:58 +0300

    Update MultipleSelect.jsx

--- src/components/UI/MultipleSelect.jsx
+++ src/components/UI/MultipleSelect.jsx
@@ -74,7 +74,7 @@ const MultipleSelect = ({ label = '', options = [], initValue = [], onChange = (
                         </MenuItem>
                     ))}
                 <MenuItem>
-                        <Button onClick={handleClose} variant="contained" color="primary">
+                        <Button onClick={handleClose} variant="contained" color=theme[themestate].button>
                             Ok
                         </Button>
                     </MenuItem>
