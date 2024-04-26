63aee9203097e36067a62c835102c420808ee17e
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-04-26 23:33:26 +0300

    Update BlockHeader.jsx

--- src/components/UI/Headers/BlockHeader.jsx
+++ src/components/UI/Headers/BlockHeader.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import DivisionLine from "../DivisionLine";
+import {tg, useTelegram} from "../../../hooks/useTelegram";
 
 const BlockHeader = ({text}) => {
+    const { tg } = useTelegram();
+    tg.setHeaderColor('var(--tg-theme-bg-color)');
     return (
         <div>
             <h2 style={{
