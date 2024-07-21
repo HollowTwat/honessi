3628d242a1494ca829bfee6b76c49971bc6fa9cd
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-07-21 18:37:46 +0300

    debug_useTNVED_created

--- src/hooks/useTNVED.js
+++ src/hooks/useTNVED.js
@@ -1,54 +1,78 @@
-import {
-    useState
-} from 'react';
+import { useState, useEffect } from 'react';
 
-const useFetchTNVEDData = () => {
+const useTNVED = (clothesType, materials, sex) => {
+  const [hsCode, setHsCode] = useState(null);
 
-    const [error, setError] = useState(null);
-    const {
-        REACT_APP_API_KEY
-    } = process.env;
-
-
-    const getData = async (type, material, sex) => {
-
-        const url = "";
-
-        try {
-            const response = await fetch(url, {
-                method: "POST",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                    "Authorization": `Bearer ${"Token " + REACT_APP_API_KEY}`
-                },
-                body: JSON.stringify({
-                    query: type + material + sex
-                })
-            });
-
-            const data = await response.json();
-
-            return {
-                tnved: data.tnved
-            }
-        } catch (error) {
-            setError(error);
-            return {
-                tnved: '321'
-            }
-        }
-    }
-
-    const handleTHVEDInfo = async (type, material, sex) => {
-        return getData(type, material, sex)
+  useEffect(() => {
+    const fetchHsCode = async () => {
+      if (clothesType && materials && sex) {
+        // Simulated delay to mimic an API call
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        setHsCode('321');
+      }
     };
 
-    return {
-        handleTHVEDInfo,
-        error
-    };
+    fetchHsCode();
+  }, [clothesType, materials, sex]);
+
+  return hsCode;
 };
 
-export default useFetchTNVEDData;
+export default useTNVED;
+
+
+
+// import {
+//     useState
+// } from 'react';
+
+// const useFetchTNVEDData = () => {
+
+//     const [error, setError] = useState(null);
+//     const {
+//         REACT_APP_API_KEY
+//     } = process.env;
+
+
+//     const getData = async (type, material, sex) => {
+
+//         const url = "";
+
+//         try {
+//             const response = await fetch(url, {
+//                 method: "POST",
+//                 mode: "cors",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                     "Accept": "application/json",
+//                     "Authorization": `Bearer ${"Token " + REACT_APP_API_KEY}`
+//                 },
+//                 body: JSON.stringify({
+//                     query: type + material + sex
+//                 })
+//             });
+
+//             const data = await response.json();
+
+//             return {
+//                 tnved: data.tnved
+//             }
+//         } catch (error) {
+//             setError(error);
+//             return {
+//                 tnved: '321'
+//             }
+//         }
+//     }
+
+//     const handleTHVEDInfo = async (type, material, sex) => {
+//         return getData(type, material, sex)
+//     };
+
+//     return {
+//         handleTHVEDInfo,
+//         error
+//     };
+// };
+
+// export default useFetchTNVEDData;
