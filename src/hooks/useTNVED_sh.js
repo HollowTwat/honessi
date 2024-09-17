a20ebd6f99e4623457d11fb5cba3e3ff00836f17
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-09-18 00:53:08 +0300

    Create useTNVED_sh.js

---
+++ src/hooks/useTNVED_sh.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+const useTNVED_s = (type, materials, sex) => {
+  const [hsCode, setHsCode] = useState(null);
+
+  useEffect(() => {
+    const fetchHsCode = async () => {
+      if (type && materials && sex) {
+        const url = `https://honessi-production.up.railway.app/api/TelegramHonessy/GetTnved?input=${clothesType},${materials},${sex}`
+
+        // Simulate a delay to mimic an API call
+        const response = await fetch(url, {
+            method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                }
+        });
+        const data = await response.text()
+        const strippedData = data.replace(/"/g, '');
+        setHsCode(strippedData); // Static value for debugging // Static value for debugging
+      }
+    };
+
+    fetchHsCode();
+  }
+  , [clothesType, materials, sex]);
+
+  return hsCode;
+};
+
+export default useTNVED_s;
