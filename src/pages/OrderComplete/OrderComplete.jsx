c7073911af37bd7f31f4bb50f662af3fba9b13e3
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-09-20 01:19:55 +0300

    Update OrderComplete.jsx

--- src/pages/OrderComplete/OrderComplete.jsx
+++ src/pages/OrderComplete/OrderComplete.jsx
@@ -38,18 +38,23 @@ const OrderComplete = () => {
                     <h1>Тестовый режим</h1>
                     <h4>Спасибо что участвуете в тестировании нашего нового бота</h4>
                 </div>
-                <h4 style={{textAlign: 'center', marginTop: 50}}>
-                    Номер заказа №{num}<br/>Скоро с Вами свяжется наш менеджер!
+                <h3 style={{textAlign: 'center', marginTop: 50}}>
+                    Номер заказа №{num}
+                </h3>
+                <h4 style={{textAlign: 'center'}}>
+                    Скоро с Вами свяжется наш менеджер!
                 </h4>
+                <h5 style={{textAlign: 'center', marginTop: 180}}>
+                    Для оперативной связи с разработкой напишите в канал<br/>https://t.me/KotOps_dev
+                </h5>
+                
 
 
             </div>
             <div style={{position: 'fixed', padding: 20, bottom: 0}}>
                 <Button onClick={handleToMainPage}>Сделать новый заказ</Button>
                 <Button onClick={handleCloseWebApp}>Вернуться в Telegram</Button>
-                <h5 style={{textAlign: 'center', marginTop: 20}}>
-                    Для оперативной связи с разработкой напишите в канал<br/>https://t.me/KotOps_dev
-                </h5>
+                
             </div>
         </div>
     );
